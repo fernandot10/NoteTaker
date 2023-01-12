29d56f359f65c53aaceb79763a0b57850d5038d4
fernandot10 <113951371+fernandot10@users.noreply.github.com>
2023-01-12 13:54:35 -0500

    edited server.js

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-const path = require ('path');
-
-
-const apiRoutes = require ('./routes/apiRoutes.js');
-const htmlRoutes = require ('./routes/htmlRoutes.js');
-
+const path = require('path');
+const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
-
-app.use (express.json());
-app.use (express.urlencoded({ extended: true }));
-app.use (express.static(path.join(__dirname, ' public ')));
-
-
-app.use(apiRoutes);
-app.use(htmlRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/api', api);
 
 
-app.listen(PORT, () => {
-    console.log(`listening in on PORT: ${PORT}`);
-});
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+app.listen(PORT, () =>
+  console.log(`listening in on PORT: ${PORT}`)
+);
